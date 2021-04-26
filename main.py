180bc067eeeb51fd1618580a2e343de0714ec130
brud <brudloord@yandex.ru>
2021-04-26 20:05:38 +0300

    zasxcvbnm,./

--- main.py
+++ main.py
@@ -88,12 +88,8 @@ def handle_dialog(req, res):
     # Если он написал 'ладно', 'куплю', 'покупаю', 'хорошо', 
     # то мы считаем, что пользователь согласился.
     # Подумайте, всё ли в этом фрагменте написано "красиво"?
-    if req['request']['original_utterance'].lower() in [
-        'ладно',
-        'куплю',
-        'покупаю',
-        'хорошо'
-    ]:
+    if (req['request']['original_utterance'].lower() in ['ладно', 'куплю', 'покупаю', 'хорошо'] or
+            'куп' in req['request']['original_utterance'].lower()):
         # Пользователь согласился, прощаемся.
         res['response']['text'] = 'Слона можно найти на Яндекс.Маркете!'
         res['response']['end_session'] = True
@@ -133,4 +129,4 @@ def get_suggests(user_id):
 
 if __name__ == '__main__':
     port = int(os.environ.get("PORT", 5000))
-    app.run(host='0.0.0.0', port=port)
+    app.run(port=port)
